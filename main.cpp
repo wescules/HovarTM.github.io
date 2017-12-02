79b44ae6aa1a30c7e09142b9c2ef7ecafa374dc1
Wesley Andrade <wafermonster@yahoo.com>
2017-12-01 21:04:44 -0600

    added vectors
    a

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<string>
+#inlcude <vector>
 #include "suicide.h"
 
 using namespace std;
 
 int main(){
-	int i = 2, j[666] = {0,1};
+	int i = 2;
+	vector<int> ar;
+	ar.push_back(2);
+	ar.push_back(1);
+	
 	print(begin(j),end(j));
 	cout << endl;
+	
 	print2(j,end(j)-begin(j));
 	print3(j);
 	return 0;
